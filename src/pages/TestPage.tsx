0b6842a954c4cd269c4232527396e591a274cdb8
Igor Marusych <progdevtoritollo@gmail.com>
2023-10-31 23:07:38 +0200

    init basic structure of project

---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,14 @@
+import TestTournament from "../entities/testComponents/TestTournament"
+
+
+function TestPage() {
+
+  return (
+    <>
+      <h1>Test Page</h1>
+      <TestTournament />
+    </>
+  )
+}
+
+export default TestPage
