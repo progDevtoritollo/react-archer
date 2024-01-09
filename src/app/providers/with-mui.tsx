10d8efb8885f11dfafd3f2cd5e36ac095c10224a
Igor Marusych <progdevtoritollo@gmail.com>
2024-01-09 16:20:20 +0100

    del strict mode & add cons.log for mui, store

--- src/app/providers/with-mui.tsx
+++ src/app/providers/with-mui.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from '@mui/material'
 
 import theme from '@/app/config/theme'
-console.log(theme)
+
+console.log(theme, 'them MaterialUI')
+
 export const withMIU = (component: () => JSX.Element) => () => <ThemeProvider theme={theme}>{component()}</ThemeProvider>
--- src/app/providers/with-store.tsx
+++ src/app/providers/with-store.tsx
@@ -1,8 +1,6 @@
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 // I don't like that we pass store here but whatever
-import { store } from '@/app/store';
+import { store } from '@/app/store'
+console.log(store, 'reduxToolKit')
 
-export const withStore = (component: () => JSX.Element) => () =>
-	<Provider store={store}>
-		{component()}
-	</Provider>;
+export const withStore = (component: () => JSX.Element) => () => <Provider store={store}>{component()}</Provider>
