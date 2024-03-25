beefe389639e389c8a523887584a2c684932d446
Igor Marusych <progdevtoritollo@gmail.com>
2024-03-25 14:58:29 +0100

    AR- 30  change Title & logo app / fix bug
    Fix bug “change state before component loaded”

--- src/pages/EntererComponent.tsx
+++ src/pages/EntererComponent.tsx
@@ -1,31 +1,52 @@
-import { Suspense, lazy, Fragment } from 'react'
-import { useSelector } from 'react-redux'
+import { Suspense, Fragment, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 import PageLoader from '@/features/PageLoader'
-const AuthRouter = lazy(() => import('@/app/router/AuthRouter'))
+import AuthRouter from '@/app/router/AuthRouter'
 import AppRouter from '@/app/router/AppRouter'
 import type { RootState } from '@/app/store'
 
+import { SetLoggedIn } from '@/entities/session/model/slice'
+import { URL_GET_IS_AUTH } from '@/app/config/config_API'
+
+const getAuthCheck = async () => {
+	const response = await axios.get(URL_GET_IS_AUTH, {
+		withCredentials: true,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+	return response.data
+}
+
 const EntererComponent: React.FC = () => {
 	const isLoggedIn = useSelector((state: RootState) => state.session.isLoggedIn)
+	const dispatch = useDispatch()
 
-	if (isLoggedIn)
-		return (
-			<Fragment>
-				<Suspense fallback={<PageLoader />}>
-					<AppRouter />
-				</Suspense>
-			</Fragment>
-		)
-	else {
-		return (
-			<Fragment>
-				<Suspense fallback={<PageLoader />}>
-					<AuthRouter />
-				</Suspense>
-			</Fragment>
-		)
+	const { isError, isPending, isSuccess } = useQuery({
+		queryKey: ['getHello'],
+		queryFn: getAuthCheck,
+	})
+
+	useEffect(() => {
+		if (isSuccess) {
+			dispatch(SetLoggedIn(true))
+		}
+		if (isError) {
+			dispatch(SetLoggedIn(false))
+		}
+	}, [isSuccess, isError, dispatch])
+
+	if (isPending) {
+		return <PageLoader />
 	}
+	return (
+		<Fragment>
+			<Suspense fallback={<PageLoader />}>{isLoggedIn ? <AppRouter /> : <AuthRouter />}</Suspense>
+		</Fragment>
+	)
 }
 
 export default EntererComponent
--- src/pages/auth/handle-auth/ui/index.tsx
+++ src/pages/auth/handle-auth/ui/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useState, useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
@@ -9,39 +9,38 @@ import { SetLoggedIn } from '@/entities/session/model/slice'
 
 import API_URL from '@/app/config/config_API'
 
+const getParam = () => {
+	const urlParams = new URLSearchParams(window.location.search)
+	const codeParamEncoded = urlParams.get('code')
+	if (typeof codeParamEncoded === 'string') {
+		const codeParamDecoded = encodeURIComponent(codeParamEncoded)
+		return codeParamDecoded
+	}
+}
+
+const getTokenUrl = async () => {
+	const codeParam = getParam()
+	const response = await axios.get(API_URL + `/auth/google/callback?code=${codeParam}`, {
+		withCredentials: true,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+	return response.data
+}
+
 export const HandleAuth: FC = () => {
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
+	const { isError, isSuccess } = useQuery({ queryKey: ['token'], queryFn: getTokenUrl })
 
-	const getParam = () => {
-		const urlParams = new URLSearchParams(window.location.search)
-		const codeParamEncoded = urlParams.get('code')
-		if (typeof codeParamEncoded === 'string') {
-			const codeParamDecoded = encodeURIComponent(codeParamEncoded)
-			return codeParamDecoded
+	useEffect(() => {
+		if (isSuccess) {
+			//* make here isAuth request here '/api/hello'?
+			dispatch(SetLoggedIn(true))
+			navigate('/user/settings')
 		}
-	}
-
-	const getTokenUrl = async () => {
-		const codeParam = getParam()
-		const response = await axios.get(API_URL + `/auth/google/callback?code=${codeParam}`, {
-			withCredentials: true,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		})
-		return response.data
-	}
-
-	const { status, isError, isSuccess } = useQuery({ queryKey: ['token'], queryFn: getTokenUrl })
-
-	if (isSuccess) {
-		console.log(isSuccess, status)
-		dispatch(SetLoggedIn(true))
-		navigate('/user/settings')
-	}
-
-	console.log(status)
+	}, [isSuccess, dispatch, navigate])
 
 	if (isError) return <div>Error</div>
 
