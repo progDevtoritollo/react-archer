9ddbbe417799cc41d3a62eeae76a32f3bfbd2a12
Igor Marusych <progdevtoritollo@gmail.com>
2024-04-17 22:04:15 +0200

    AR- 41 change url for post round

--- src/entities/contest/api/post-round.ts
+++ src/entities/contest/api/post-round.ts
@@ -2,6 +2,6 @@ import { apiClient } from '@/shared/api/base'
 import { roundRdo } from './rdo/round.rdo'
 
 export const postRound = async (body: roundRdo) => {
-	const res = await apiClient.post<roundRdo>('/api/rounds/me/save', body)
+	const res = await apiClient.post<roundRdo>('/api/rounds/save', body)
 	return res
 }
